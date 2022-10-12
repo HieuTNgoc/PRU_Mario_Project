ff06cd5702b790414c02a663519071370e7ac5d3
Tran Hieu <hieutnhe153199@fpt.edu.vn>
2022-10-13 04:34:09 +0700

    Add Move Left Right

--- Assets/Pixel Adventure 1/Assets/Script/MainPlayerScript.cs
+++ Assets/Pixel Adventure 1/Assets/Script/MainPlayerScript.cs
@@ -4,23 +4,57 @@ using UnityEngine;
 
 public class MainPlayerScript : MonoBehaviour
 {
-    public float Speed;
-    public bool OnGround = true;
-    public bool Turn  = false;
+    public float velocity;
 
-    private Animator animation;
+    private float Speed = 0;
+    private bool OnGround = true;
+    private bool Turn  = false;
+    private bool TurnRight = true;
+    private Rigidbody2D rigidbody2d;
+
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        animation = GetComponent<Animator>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        animation.SetFloat("Speed", Speed);
-        animation.SetBool("OnGround", OnGround);
-        animation.SetBool("Turn", Turn);
+        animator.SetFloat("Speed", Speed);
+        animator.SetBool("OnGround", OnGround);
+        animator.SetBool("Turn", Turn);
+        JumpUp();
+    }
 
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    void Move()
+    {
+        float keyLeftRigth = Input.GetAxis("Horizontal");
+        rigidbody2d.velocity = new Vector2(velocity*keyLeftRigth, rigidbody2d.velocity.y);
+        Speed = Mathf.Abs(velocity*keyLeftRigth);
+        if (keyLeftRigth > 0 && !TurnRight) CheckDirection(); 
+        if (keyLeftRigth < 0 && TurnRight) CheckDirection();
+    }
+
+    void CheckDirection()
+    {
+        TurnRight = !TurnRight;
+        Vector2 direction = transform.localScale;
+        direction.x *= -1;
+        transform.localScale = direction;
+    }
+
+    void JumpUp()
+    {
+        if (Input.GetKeyDown(KeyCode.X) && OnGround == true)
+        {
+            print("nhau");
+        }
     }
 }
